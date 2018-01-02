08c9e29700e1569386dec12413362edc38f953e1
Brendan Higgins <bhiggins033@gmail.com>
2018-01-02 15:04:20 -0500

    Update entry file and root component

--- frontend/venn.jsx
+++ frontend/venn.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { login, signup, logout } from './actions/session_actions';
+import Root from './components/root';
+// import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
 
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // testing purposes
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Venn!</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
