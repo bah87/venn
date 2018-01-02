cad6fde1c8d95646320bf50d3bf076f248b4278e
Brendan Higgins <bhiggins033@gmail.com>
2018-01-02 12:19:01 -0500

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,38 @@
 #
 
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, :session_token, :password_digest, presence: true
+  validates :email, :session_token, unique: true
+  validates :password, length { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
