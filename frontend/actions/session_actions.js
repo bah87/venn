494cec11b85b608cb73c60270620a67a91049ee6
Brendan Higgins <bhiggins033@gmail.com>
2018-01-02 15:10:38 -0500

    Complete greeting container and rename user to currentUser in actions/reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -6,7 +6,7 @@ export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 export const receiveCurrentUser = user => {
   return {
     type: RECEIVE_CURRENT_USER,
-    user
+    currentUser
   };
 };
 
