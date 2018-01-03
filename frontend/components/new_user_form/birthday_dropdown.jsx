315c23cc9ebc4626e11bf28993fcc47ba54a65d4
Brendan Higgins <bhiggins033@gmail.com>
2018-01-03 12:26:23 -0500

    Birthday state updating

--- frontend/components/new_user_form/birthday_dropdown.jsx
+++ frontend/components/new_user_form/birthday_dropdown.jsx
@@ -10,13 +10,21 @@ class BirthdayDropdown extends React.Component {
     ];
     this.days = [...Array(32).keys()].slice(1);
     this.years = [...Array(2019).keys()].slice(1905);
-    this.update = this.update.bind(this);
+    this.updateMonth = this.updateMonth.bind(this);
+    this.updateDay = this.updateDay.bind(this);
+    this.updateYear = this.updateYear.bind(this);
   }
 
-  update(field) {
-    return event => {
-      this.setState({ [field]: event.target.value });
-    };
+  updateMonth(event) {
+    this.setState({ month: this.months.indexOf(event.target.value) + 1 });
+  }
+
+  updateDay(event) {
+    this.setState({ day: this.days.indexOf(event.target.value) + 1 });
+  }
+
+  updateYear(event) {
+    this.setState({ year: this.years.indexOf(event.target.value) + 1 });
   }
 
   render() {
@@ -46,12 +54,24 @@ class BirthdayDropdown extends React.Component {
         }
       });
 
+    // const months = ["Month"].concat(this.months).map((month, idx) => (
+    //   <option key={idx} value={this.state.month}>{ month }</option>
+    // ));
+    //
+    // const days = ["Day"].concat(this.days).map((day, idx) => (
+    //   <option key={idx} value={this.state.day}>{ day }</option>
+    // ));
+    //
+    // const years = ["Year"].concat(this.years).map((year, idx) => (
+    //   <option key={idx} value={this.state.year}>{ year }</option>
+    // ));
+
     return (
       <div className="birthday-signup">
         <p>Birthday</p>
-        <select onChange={this.update("month")}>{ months }</select>
-        <select onChange={this.update("day")}>{ days }</select>
-        <select onChange={this.update("year")}>{ years }</select>
+        <select onChange={this.updateMonth}>{ months }</select>
+        <select onChange={this.updateDay}>{ days }</select>
+        <select onChange={this.updateYear}>{ years }</select>
       </div>
     );
   }
