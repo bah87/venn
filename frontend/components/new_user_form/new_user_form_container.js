980e0e684cf3031f8a0b5f6a3ad27acea0cc5299
Brendan Higgins <bhiggins033@gmail.com>
2018-01-03 11:11:36 -0500

    Erase commented out mapStateToProps

--- frontend/components/new_user_form/new_user_form_container.js
+++ frontend/components/new_user_form/new_user_form_container.js
@@ -2,10 +2,6 @@ import { connect } from 'react-redux';
 import { login, logout, signup } from '../../actions/session_actions';
 import NewUserForm from './new_user_form';
 
-// const mapStateToProps = state => {
-//   return {};
-// };
-
 const mapDispatchToProps = dispatch => {
   return {
     signup: user => dispatch(signup(user))
