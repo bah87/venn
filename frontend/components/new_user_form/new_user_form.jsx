9a7748f369cba2253c511dc07ba4ea45e267d400
Brendan <bhiggins033@gmail.com>
2018-01-03 00:33:18 -0500

    Add more styling to new user and session forms

--- frontend/components/new_user_form/new_user_form.jsx
+++ frontend/components/new_user_form/new_user_form.jsx
@@ -21,62 +21,64 @@ class NewUserForm extends React.Component {
   }
 
   render() {
-
-    // <label>
-    //   <input
-    //     type="text"
-    //     placeholder="First name"
-    //     value={this.state.first_name}
-    //     onChange={this.update("first_name")}
-    //   />
-    // </label>
-    //
-    // <label>
-    //   <input
-    //     type="text"
-    //     placeholder="Last name"
-    //     value={this.state.last_name}
-    //     onChange={this.update("last_name")}
-    //   />
-    // </label>
-
     return (
       <main className="landing-page-main">
-        <div className="signup-static-content">
-          <h4>Connect with friends and the world around you on Venn.</h4>
-          <ul>
-            <li>See photos and updates from friends in News Feed.</li>
-            <li>Share what's new in your life on your Timeline.</li>
-            <li>Find more of what you're looking for with Venn Search.</li>
-          </ul>
-        </div>
-
         <div className="signup-container">
-          <form onSubmit={this.handleSubmit} className="signup-form">
-            <h3>Sign Up</h3>
-            <p>It's free and always will be.</p>
+          <div className="signup-static-content">
+            <h4>Connect with friends and the world around you on Venn.</h4>
+            <ul>
+              <li>See photos and updates from friends in News Feed.</li>
+              <li>Share what's new in your life on your Timeline.</li>
+              <li>Find more of what you're looking for with Venn Search.</li>
+            </ul>
+          </div>
 
+          <div className="signup-form-container">
+            <form onSubmit={this.handleSubmit} className="signup-form">
+              <h3>Sign Up</h3>
+              <p>It's free and always will be.</p>
 
-            <label>
-              <input
-                type="text"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.update("email")}
-              />
-            </label>
+              <div className="first-and-last-name">
+                <label>
+                  <input
+                    type="text"
+                    placeholder="First name"
+                    value={this.state.first_name}
+                    onChange={this.update("first_name")}
+                  />
+                </label>
 
-            <label>
-              <input
-                type="password"
-                placeholder="New password"
-                value={this.state.password}
-                onChange={this.update("password")}
-              />
-            </label>
+                <label>
+                  <input
+                    type="text"
+                    placeholder="Last name"
+                    value={this.state.last_name}
+                    onChange={this.update("last_name")}
+                  />
+                </label>
+              </div>
 
-            <input type="submit" value="Create Account" />
-          </form>
+              <label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.update("email")}
+                />
+              </label>
+
+              <label>
+                <input
+                  type="password"
+                  placeholder="New password"
+                  value={this.state.password}
+                  onChange={this.update("password")}
+                />
+              </label>
+
+              <input id="signup-btn" type="submit" value="Create Account" />
+            </form>
+          </div>
         </div>
       </main>
 
