9e8545dd9b08de5dcec2b62236a687b21dbc9956
Brendan Higgins <bhiggins033@gmail.com>
2018-01-04 15:07:58 -0500

    Add classNames to post index item elements

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -5,8 +5,16 @@ const PostIndexItem = ({ post, deletePost }) => {
     <li className="post-item">
 
       <header className="post-item-header">
-        <div>
-          
+        <p className="post-item-profile-pic"></p>
+        <div className="post-item-header-right">
+          <div>
+            <p className="post-item-name">Brendan Higgins</p>
+            <button
+              className="post-item-delete-btn"
+              onClick={() => deletePost(post.id)}>Delete</button>
+          </div>
+
+          <p className="post-item-date">{"January 1 at 5:27pm"}</p>
         </div>
       </header>
 
@@ -15,12 +23,15 @@ const PostIndexItem = ({ post, deletePost }) => {
       </main>
 
       <footer className="post-item-footer">
-
+        <div className="like">
+          <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
+          <p>Like</p>
+        </div>
+        <div className="comment">
+          <i className="fa fa-comment-o" aria-hidden="true"></i>
+          <p>Comment</p>
+        </div>
       </footer>
-
-
-
-      <button onClick={() => deletePost(post.id)}>Delete</button>
     </li>
   );
 };
