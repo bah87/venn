670ece437b6071dec45fdf4067c050db448255a9
Brendan <bhiggins033@gmail.com>
2018-01-03 23:46:02 -0500

    Posts reducer

---
+++ frontend/reducers/posts_reducer.js
@@ -0,0 +1,24 @@
+import {
+  RECEIVE_POST,
+  RECEIVE_ALL_POSTS,
+  REMOVE_POST
+} from '../actions/post_actions';
+import merge from 'lodash/merge';
+
+const PostsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_POSTS:
+      return merge({}, action.posts);
+    case RECEIVE_POST:
+      return merge({}, state, { [action.post.id]: action.post });
+    case REMOVE_POST:
+      let newState = merge({}, state);
+      delete newState[action.postId];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default PostsReducer;
