35bc93083c992f0a7f79415458d03e658e16b9c8
Brendan <bhiggins033@gmail.com>
2018-01-04 00:14:13 -0500

    Post index container

---
+++ frontend/components/posts/post_index_container.js
@@ -0,0 +1,29 @@
+import { connect } from 'react-redux';
+import PostIndex from './post_index';
+import {
+  fetchPosts,
+  createPost,
+  updatePost,
+  deletePost
+} from '../../actions/post_actions';
+
+const mapStateToProps = state => {
+  debugger
+  return {
+    posts: state.entities.posts
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchPosts: () => dispatch(fetchPosts()),
+    createPost: post => dispatch(createPost(post)),
+    updatePost: post => dispatch(updatePost(post)),
+    deletePost: id => dispatch(deletePost(id))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostIndex);
