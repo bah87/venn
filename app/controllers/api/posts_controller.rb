b5c3b067379c7008cf468ef7ecd6f91256e2e58f
Brendan Higgins <bhiggins033@gmail.com>
2018-01-05 17:21:29 -0500

    Update posts#show_profile to return correct posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -6,7 +6,13 @@ class Api::PostsController < ApplicationController
   end
 
   def show_profile
-    @posts = User.find(params[:user_id]).posts
+    my_posts_no_recipient = User.find(params[:user_id]).posts.reject do |post|
+      !post.recipient_id.nil
+    end
+
+    posts_to_me = Post.find_by(recipient_id: current_user.id)
+
+    @posts = my_posts_no_recipient + posts_to_me
     render :index
   end
 
