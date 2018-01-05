37d57ac70364fafa372b784e01d99f8dcf9b8a8a
Brendan Higgins <bhiggins033@gmail.com>
2018-01-05 10:16:37 -0500

    Add user actions (regular and thunk) for fetching friends

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,6 +1,7 @@
 import * as UserApiUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
+export const RECEIVE_FRIENDS = 'RECEIVE_FRIENDS';
 
 export const receiveUser = user => {
   return {
@@ -9,8 +10,21 @@ export const receiveUser = user => {
   };
 };
 
+export const receiveFriends = friends => {
+  return {
+    type: RECEIVE_FRIENDS,
+    friends
+  };
+};
+
 export const fetchUser = id => dispatch => {
   return UserApiUtil.fetchUser(id).then(user => {
     dispatch(receiveUser(user));
+  });
+};
+
+export const fetchFriends = () => dispatch => {
+  return UserApiUtil.fetchFriends().then(friends => {
+    dispatch(receiveFriends(friends));
   });
 };
