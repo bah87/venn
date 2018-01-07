0c08fb167e881e34a3c33488830eff09649078ef
Brendan <bhiggins033@gmail.com>
2018-01-07 13:28:12 -0500

    Added and styled link to profile from newsfeed

--- frontend/components/main_pages/news_feed.jsx
+++ frontend/components/main_pages/news_feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostIndexContainer from '../posts/post_index_container';
 import PostFormContainer from '../posts/post_form_container';
+import { Link } from 'react-router-dom';
 
 class NewsFeed extends React.Component {
   componentDidMount() {
@@ -8,14 +9,39 @@ class NewsFeed extends React.Component {
   }
 
   render() {
+    const currentUser = this.props.currentUser;
     return (
-      <div>
-        <PostFormContainer
-          page={"profile"}
-          user={ this.props.user }
-          recipient={ false }
-        />
-        <PostIndexContainer page={"feed"} friends={this.props.friends} />
+      <div className="newsfeed-main">
+        <div className="newsfeed-sidebar">
+          <ul>
+            <li>
+              <Link
+                to={`/profile/${currentUser.id}`}
+                style={{ textDecoration: 'none'}}>
+                <div className="newsfeed-li-name">
+                  <img className="newsfeed-sidebar-profile-pic"
+                    src={window.staticImages[currentUser.profile_pic_url.split('.')[0]]}
+                    />
+                  <p>
+                    {`${currentUser.first_name} ${currentUser.last_name}`}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="newsfeed-post-index">
+          <PostFormContainer
+            page={"profile"}
+            user={ this.props.user }
+            recipient={ false }
+            />
+          <PostIndexContainer page={"feed"} friends={this.props.friends} />
+        </div>
+
+        <div className="newsfeed-trending">
+        </div>
       </div>
     );
   }
