5cc339af5fa339dd914de29e64ff56915214cfb2
Brendan <bhiggins033@gmail.com>
2018-01-08 15:19:26 -0500

    Style profile pic update button

--- frontend/components/main_pages/profile_picture.jsx
+++ frontend/components/main_pages/profile_picture.jsx
@@ -7,6 +7,21 @@ class ProfilePicture extends React.Component {
       imageFile: null,
       imageUrl: null
     };
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.updateFile = this.updateFile.bind(this);
+    // this.cancelUpload = this.cancelUpload.bind(this);
+  }
+
+  handleFile(event) {
+
+  }
+
+  cancelUpload() {
+
+  }
+
+  handleSubmit() {
+
   }
 
   render() {
@@ -23,7 +38,17 @@ class ProfilePicture extends React.Component {
         </div>
 
         <div className="profile-pic-btn">
-          
+          <label
+            for="profile-pic-upload"
+            className="prof-pic-btn-label">
+            <i className="fa fa-camera" aria-hidden="true"></i>
+            <p>Update Profile Picture</p>
+          </label>
+          <input
+            id="profile-pic-upload"
+            type="file"
+            onChange={this.updateFile}
+          />
         </div>
       </div>
     );
