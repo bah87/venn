b201e5319406d4f6a748cadb99a683666906922a
Brendan Higgins <bhiggins033@gmail.com>
2018-01-09 16:40:40 -0500

    Add comment model

---
+++ db/migrate/20180109213040_create_api_comments.rb
@@ -0,0 +1,6 @@
+class CreateApiComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :api_comments do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108055346) do
+ActiveRecord::Schema.define(version: 20180109213939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "body", null: false
