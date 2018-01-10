b2efff707fc3d163d3688eb091548ef84ac9b82c
Brendan <bhiggins033@gmail.com>
2018-01-09 21:39:15 -0500

    Add actions to comments api util

--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -11,3 +11,26 @@ export const deleteComment = commentId => {
     url: `/api/comments/${commentId}`
   });
 };
+
+export const createComment = comment => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/comments',
+    data: { comment }
+  });
+};
+
+export const fetchComment = commentId => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/comments${commentId}`
+  });
+};
+
+export const updateComment = comment => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/comments/${comment.id}`,
+    data: { comment }
+  });
+};
