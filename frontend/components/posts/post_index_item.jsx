45020c3686cd503d0e33614fbc1f780fb4994731
Brendan Higgins <bhiggins033@gmail.com>
2018-01-10 11:52:59 -0500

    Fix no author issue on post index item

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -9,72 +9,74 @@ const PostIndexItem = ({ toggleEditPostModal, currentUser,
   author, post, modal, deletePost, updatePost, deletePostPhoto }) => {
   let date = DateFormat(post.updated_at);
 
-  return (
-    <li className="post-item">
+  if (author) {
+    return (
+      <li className="post-item">
 
-      <header className="post-item-header">
-        <Link to={`/profile/${author.id}`}>
-          <img className="post-item-profile-pic"
-            src={author.profile_pic_url}
-            />
-        </Link>
+        <header className="post-item-header">
+          <Link to={`/profile/${author.id}`}>
+            <img className="post-item-profile-pic"
+              src={author.profile_pic_url}
+              />
+          </Link>
 
-        <div className="post-item-header-right">
-          <div className="post-item-name-and-options">
-            <Link
-              to={`/profile/${author.id}`} style={{ textDecoration: 'none'}}>
-              <p className="post-item-name">
-                {`${author.first_name} ${author.last_name}`}
-              </p>
-            </Link>
+          <div className="post-item-header-right">
+            <div className="post-item-name-and-options">
+              <Link
+                to={`/profile/${author.id}`} style={{ textDecoration: 'none'}}>
+                <p className="post-item-name">
+                  {`${author.first_name} ${author.last_name}`}
+                </p>
+              </Link>
 
-            <PostDropdown
-              postId={ post.id }
-              currentUser={ currentUser }
-              author={ author }
-              toggleEditPostModal={ toggleEditPostModal }
-              deletePost={ deletePost }
-            />
-            <EditPostForm
-              modal={ modal }
-              post={ post }
-              toggleModal={ toggleEditPostModal }
-              updatePost={ updatePost }
-              deletePostPhoto={ deletePostPhoto }
-              currentUser={ author }
-            />
+              <PostDropdown
+                postId={ post.id }
+                currentUser={ currentUser }
+                author={ author }
+                toggleEditPostModal={ toggleEditPostModal }
+                deletePost={ deletePost }
+              />
+              <EditPostForm
+                modal={ modal }
+                post={ post }
+                toggleModal={ toggleEditPostModal }
+                updatePost={ updatePost }
+                deletePostPhoto={ deletePostPhoto }
+                currentUser={ author }
+              />
+            </div>
+
+            <div className="post-item-date-container">
+              <Link to={`/users/${post.author_id}/posts/${post.id}`}
+                style={{ textDecoration: 'none'}}>
+                <p className="post-item-date">
+                  { date }
+                </p>
+              </Link>
+            </div>
           </div>
+        </header>
 
-          <div className="post-item-date-container">
-            <Link to={`/users/${post.author_id}/posts/${post.id}`}
-              style={{ textDecoration: 'none'}}>
-              <p className="post-item-date">
-                { date }
-              </p>
-            </Link>
+        <main className="post-item-body">
+          <p>{ post.body }</p>
+          <PostImage form={false} imageUrl={post.image_url} />
+        </main>
+
+        <footer className="post-item-footer">
+          <div className="like-btn">
+            <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
+            <p>Like</p>
           </div>
-        </div>
-      </header>
-
-      <main className="post-item-body">
-        <p>{ post.body }</p>
-        <PostImage form={false} imageUrl={post.image_url} />
-      </main>
-
-      <footer className="post-item-footer">
-        <div className="like-btn">
-          <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
-          <p>Like</p>
-        </div>
-        <div
-          onClick={() => document.getElementById("comment-input-focus").focus()}
-          className="comment-btn">
-          <i className="fa fa-comment-o" aria-hidden="true"></i>
-          <p>Comment</p>
-        </div>
-      </footer>
-    </li>
-  );
+          <div
+            onClick={() => document.getElementById("comment-input-focus").focus()}
+            className="comment-btn">
+            <i className="fa fa-comment-o" aria-hidden="true"></i>
+            <p>Comment</p>
+          </div>
+        </footer>
+      </li>
+    );
+  } else { return null; }
 };
 
 export default PostIndexItem;
