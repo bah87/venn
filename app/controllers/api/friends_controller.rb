4bd21228c578fd41bebbc5280cdcd3ffb99bc380
Brendan <bhiggins033@gmail.com>
2018-01-11 22:56:22 -0500

    Add friends controller

---
+++ app/controllers/api/friends_controller.rb
@@ -0,0 +1,32 @@
+class Api::FriendsController < ApplicationController
+  def request_friend
+    @request = Friend.new(friend_params)
+    @request.requestor_id = current_user.id
+    @request.status = 'PENDING'
+    if @request.save!
+      render :show
+    else
+      render json: @request.errors.full_messages
+    end
+  end
+
+  def accept_friend
+    @request = current_user.friend_requests.find_by(requestor_id: params[:requestor_id])
+    @request.status = 'ACCEPTED'
+    if @request.save!
+      render :show
+    else
+      render json: @request.errors.full_messages
+    end
+  end
+
+  def reject_friend
+    @request = current_user.friend_requests.find_by(requestor_id: params[:requestor_id])
+    @request.destroy!
+  end
+
+  private
+  def friend_params
+    params.require(:friend).permit(:id, :receiver_id)
+  end
+end
